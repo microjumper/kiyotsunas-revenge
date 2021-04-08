5f7a165484de52f3bc1c06e366635c0e25495cd8
Salvatore Sammarco <microjmpr@gmail.com>
2021-04-09 00:44:20 +0200

    Attack

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,19 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public LayerMask enemyLayers;
+    public Transform attackPoint;
+
+    private Animator animator;
+    
+    [SerializeField]
+    private float attackRange = 1.25f;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();    
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +38,14 @@ public class Player : MonoBehaviour
 
     public void OnAttack()
     {
-        Debug.Log("Attack");
+        animator.SetTrigger("Attack");
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        System.Array.ForEach<Collider2D>(enemies, (enemy) => Debug.Log("Colpito"));
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
