19ba29a4086fe767ac8d95b5c424b66e2e05a923
Salvatore Sammarco <microjmpr@gmail.com>
2021-04-09 04:15:06 +0200

    Basic movements

---
+++ Assets/Scripts/Constraint.cs
@@ -0,0 +1,7 @@
+public static class Constraint
+{
+    public static readonly float TOP = 0;
+    public static readonly float RIGHT = 8;
+    public static readonly float LEFT = -8;
+    public static readonly float BOTTOM = -4;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,13 +8,19 @@ public class Player : MonoBehaviour
     public LayerMask enemyLayers;
     public Transform attackPoint;
 
+    private new Rigidbody2D rigidbody;
     private Animator animator;
-    
-    [SerializeField]
-    private float attackRange = 1.25f;
+
+    private readonly float attackRange = 1.25f;
+    private readonly float speed = 5f;
+    private readonly float attackRate = 0.25f;
+
+    private bool canAttack = true;
+    private Vector2 inputDirection = Vector2.zero;
 
     private void Awake()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();    
     }
 
@@ -27,25 +33,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, Constraint.LEFT, Constraint.RIGHT), Mathf.Clamp(transform.position.y, Constraint.BOTTOM, Constraint.TOP));
+    }
+
+    private void FixedUpdate()
+    {
+        if(canAttack)
+        {
+            animator.SetBool("Running", !inputDirection.Equals(Vector2.zero));
+
+            rigidbody.MovePosition((Vector2)transform.position + inputDirection * speed * Time.fixedDeltaTime);
+        }
     }
 
     public void OnMove(InputValue value)
     {
-        Debug.Log("Move");
-        Debug.Log(value.Get<Vector2>());
+        inputDirection = value.Get<Vector2>();
     }
 
     public void OnAttack()
     {
+        if(canAttack)
+        {
+            Attack();
+            canAttack = false;
+        }
+    }
+
+    private void Attack()
+    {
         animator.SetTrigger("Attack");
 
         Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-        System.Array.ForEach<Collider2D>(enemies, (enemy) => Debug.Log("Colpito"));
+        System.Array.ForEach(enemies, (enemy) => Debug.Log("Colpito"));
+
+        StartCoroutine(Cooldown());
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(attackRate);
+        canAttack = true;
     }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        if(gameObject!=null)
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
